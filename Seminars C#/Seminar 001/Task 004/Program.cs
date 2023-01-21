f7d94752a3b062b5c3ade003f00d251b3dcd4957
Sergei <yagelavichus@gmail.com>
2023-01-21 19:05:33 +0200

    Task 004

---
+++ Seminars C#/Seminar 001/Task 004/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая на вход принимает одно число (назовём его n), 
+а на выходе показывает все целые числа в промежутке от -n до n.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4 "
+2 -> "-2, -1, 0 , 1 , 2 "
+ */
+
+Console.WriteLine("Введите первое число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+for (int i = - number; i <=number; i++)
+{
+   Console.Write(i + " ,");
+}
