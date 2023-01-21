90482b1baa7cae78b04929dc80e40a24db488f9f
Sergei <yagelavichus@gmail.com>
2023-01-21 17:26:35 +0200

    Task 001 and Task 002

---
+++ Seminars C#/Seminar 001/Task 001/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт её квадрат (число умноженное на само себя).
+
+Например:
+4->16
+-3->9
+-7->49
+*/
+
+Console.WriteLine("Введите число: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
---
+++ Seminars C#/Seminar 001/Task 002/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу которая на вход принимает два числа и проверяет 
+является ли первое число квадратом второго .*/
+
+Console.WriteLine("Введите первое число");
+string numderStr1 = Console.ReadLine();
+int a = Convert.ToInt32(numderStr1);
+
+Console.WriteLine("Введите второе число");
+string numderStr2 = Console.ReadLine();
+int b = Convert.ToInt32(numderStr2);
+
+int c = a / b ;
+int d = b / a ;
+
+if ( c == b) 
+{
+    Console.WriteLine("Число является квадратом");
+}
+else 
+{
+   Console.WriteLine("Число не является квадратом"); 
+}
+
