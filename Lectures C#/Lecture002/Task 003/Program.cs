469a94d1a90d4ba6f7d80b460a2e57b52f839ee2
Sergei <yagelavichus@gmail.com>
2023-01-21 20:35:54 +0200

    Task 002 Array index

---
+++ Lectures C#/Lecture002/Task 003/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 12, 43, 4, 15, 56, 27, 38 };
+
+int n = array.Length;
+int find = 56;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
