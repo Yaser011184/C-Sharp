638ac3e11168fc3e6682424f048f413d5c48e3e6
Sergei <yagelavichus@gmail.com>
2023-01-22 21:57:49 +0200

    Seminar 002 Task 001

--- Seminars C#/Seminar 002/Task 001/Program.cs
+++ Seminars C#/Seminar 002/Task 001/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая выводит случайное число из отрезка [10, 99]
+и показывает наибольшую цифру числа.
+
+Например:
+78 -> 8
+12 -> 2
+85 -> 8
+*/
+
+/*
+1. Метод, который получает случайное число
+2. Метод, который получает максимальную цифру числа
+*/
+
+int GetRandomNumber(int leftBound, int rightBound)
+{
+    int result = 0;
+    Random rnd = new Random();
+
+    result = rnd.Next(leftBound, rightBound + 1);
+    return result;
+}
+
+int GetMaxDigitOfNumber(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+
+    if (firstDigit > secondDigit)
+
+        return firstDigit;
+
+    else
+
+        return secondDigit;
+
+}
+
+int number = GetRandomNumber(10, 99);
+int maxDigit = GetMaxDigitOfNumber(number);
+
+Console.WriteLine($"Максимальная цифра числа {number} = {maxDigit}");
+
+
