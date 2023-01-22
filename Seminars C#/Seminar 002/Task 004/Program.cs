d6ee1eb0882002d09e50b0d7b60cd4c438e3b3fa
Sergei <yagelavichus@gmail.com>
2023-01-22 23:33:38 +0200

    Task 004

---
+++ Seminars C#/Seminar 002/Task 004/Program.cs
@@ -0,0 +1,19 @@
+﻿/* напишите программу, которая принимает на вход число и проверяет кратное ли оно одновременно 7 и 23. */
+
+int GetNumber()
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+Console.WriteLine("Введите число");
+int number = GetNumber();
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Число кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Число не кратно");
+}
