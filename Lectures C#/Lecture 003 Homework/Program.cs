6ca2d69b03a3997789b3b840b4d0d6e88a560a9f
Sergei <yagelavichus@gmail.com>
2023-01-28 20:40:14 +0200

    For and max

---
+++ Lectures C#/Lecture 003 Homework/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+       Console.Write($"{arr[i]}"); 
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+      int maxPosition = i;
+
+      for (int j = i + 1; j < array.Length; j++)
+      {
+        if (array[j] > array[maxPosition]) maxPosition = j;
+      }
+
+      int temporary = array[i]; //  (int temporary) временный элемент при замене
+      array[i] = array[maxPosition]; // замена мест переменных
+      array[maxPosition] = temporary; // замена мест переменных
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+
+
+// КОД ПЕРЕДЕЛАН НА СОРТИРОВКУ МАССИВА ОТ БОЛЬШЕГО К МЕНЬШЕМУ
