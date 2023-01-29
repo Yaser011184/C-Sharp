20611c4777f2fa575f57009663056d0f5a885a21
Sergei <yagelavichus@gmail.com>
2023-01-29 12:57:22 +0200

    Task 002 Sum of number

---
+++ Home Works/Homework_seminar004/Task 002/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+
+int GetNumber(string message)
+{
+    int number = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод: ");
+        }
+    }
+    
+    return number;
+}
+
+int GetSumm(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int num = number % 10;
+        number = number / 10;
+        sum = sum + num;
+    }
+        
+    return sum;
+}
+
+int number = GetNumber("Введите число");
+Console.WriteLine(GetSumm(number));
+
