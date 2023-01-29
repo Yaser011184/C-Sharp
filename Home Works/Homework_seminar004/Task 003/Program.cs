66c63e34e7c6f46feee6ce4d469c7e69a4a4accb
Sergei <yagelavichus@gmail.com>
2023-01-29 13:06:33 +0200

    Task 003 massive

---
+++ Home Works/Homework_seminar004/Task 003/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+
+int[] GetMassive()
+{
+    int [] newMassive = new int [8];
+    Random number = new Random();
+
+    for (int i = 0; i < newMassive.Length; i++)
+    {
+       newMassive[i] = number.Next(0, 10);
+     
+    }
+    return newMassive;
+    
+}
+int [] massive = GetMassive();
+
+void PrintMassive(int [] massive)
+{
+    for (int i = 0; i < massive.Length; i++)
+    {
+       Console.Write($"{massive[i]}" + ","); 
+    }
+}
+
+PrintMassive(massive);
