43762f57f688d7b1e7c241a5f927c9fc86cb736e
Sergei <yagelavichus@gmail.com>
2023-01-29 13:52:17 +0200

    Task 002 multiple

---
+++ Seminars C#/Seminar 004/Task 002/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+    
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод:");
+        }
+    }
+
+    return Math.Abs(result);
+}
+void GetLong(int GetNumber)
+{
+   string GetText =  GetNumber.ToString();
+   int longText = GetText.Length;
+   Console.WriteLine(longText);
+}
+
+int number = GetNumber("Введите любое число:");
+GetLong(number);
---
+++ Seminars C#/Seminar 004/Task003/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение(умножить) чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+int GetNumber(string message)
+{
+    int number = 0;
+
+    while (true)
+    {
+       Console.WriteLine(message);
+
+       if (int.TryParse(Console.ReadLine(), out number) && number >= 1 )
+       {
+        break;
+       } 
+       else
+       {
+        Console.WriteLine("Ввели не число. Повторите ввод:");
+       }
+    }
+    return number;
+}
+
+int GetSumm(int number)
+{
+    int sum = 1;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum * i; 
+    }
+    return sum;
+}
+
+ int multi = GetNumber("Введите число:");
+
+int result = GetSumm(multi);
+Console.WriteLine(result);
