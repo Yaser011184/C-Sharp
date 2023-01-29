68dc0564cbadd39b5024d71fec0b49578701615f
Sergei <yagelavichus@gmail.com>
2023-01-29 13:24:54 +0200

    Task 001 sum of A

---
+++ Seminars C#/Seminar 004/Task 001/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+1. получаем число с консоли
+2. считаем сумму чисел от 1 до введенного числа
+3. выводим сумму на экран
+*/
+
+
+int GetNumber(string message) // "Введите число" сообщение
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не числою. Повторите ввод:");
+        }
+    }
+    return result;
+}
+
+int GetSumm(int number)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+
+int number = GetNumber("Введите число больше 1");
+int sum = GetSumm(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sum}");
