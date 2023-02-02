f483df69a9ceefb33646b2f56a32bb556f5ed542
Sergei <yagelavichus@gmail.com>
2023-02-02 20:23:45 +0200

    Factorial

--- Lectures C#/Lecture 004/Program.cs
+++ Lectures C#/Lecture 004/Program.cs
@@ -50,3 +50,11 @@ PrintArray(matrix);
 FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
+
+
+// int Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// Console.WriteLine(Factorial(3)); // 1*2*3 =6
