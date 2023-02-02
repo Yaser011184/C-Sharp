b0698e9cd87ccb72895aff22a680fa1ab336ecdf
Sergei <yagelavichus@gmail.com>
2023-02-02 23:14:23 +0200

    Task 005 Copy massive

---
+++ Seminars C#/Seminar 006/Task 005/Program.cs
@@ -0,0 +1,36 @@
+﻿
+/*
+Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+*/
+
+
+int[] InitArray(int message)
+{
+    int[] result = new int[message];
+    Random rnd = new Random();
+
+    for (int i = 0; i < message; i++)
+    {
+        result[i] = rnd.Next(1, 10);
+    }
+    return result;
+}
+
+int[] CopyArray(int[] startArray)
+{
+    int[] array = new int[startArray.Length];
+
+    for (int i = 0; i < startArray.Length; i++)
+    {
+        array[i] = startArray[i];
+    }
+       
+    return array;
+}
+
+
+int[] array = InitArray(10);
+Console.WriteLine(string.Join(", ", array));
+
+int[] arr = CopyArray(array);
+Console.WriteLine(string.Join(", ", arr));
