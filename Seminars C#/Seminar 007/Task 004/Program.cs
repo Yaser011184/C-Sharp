3b6596906d09d9a34cd222dc97f2fd94077f5094
Sergei <yagelavichus@gmail.com>
2023-02-06 21:41:50 +0200

    Task 004 sum on diagonale

---
+++ Seminars C#/Seminar 007/Task 004/Program.cs
@@ -0,0 +1,88 @@
+﻿/*
+Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+
+
+// метод получения числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некоректное число. Повторите ввод:");
+        }
+    }
+    return result;
+}
+
+// метод инициализирующий матрицу
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+// метод распечатывающий матрицу
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int indexSumm(int[,] matrix)
+{
+    int result = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                result = result + matrix[i,j];
+            }
+        }
+    }
+    return result;
+    
+}
+
+int countOfRows = GetNumber("Введитее кол-во строк:");
+int countOfColumns = GetNumber("Введитее кол-во столбцов:");
+int[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+PrintMatrix(matrix);
+Console.WriteLine();
+
+
+int array = indexSumm(matrix);
+Console.WriteLine($"Сумма элементов, находящихся на главной диагонали = {array}");
