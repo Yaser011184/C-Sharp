e802f4b6f6ca91dabad8e73cedf55f6d8d042e8d
Sergei <yagelavichus@gmail.com>
2023-02-06 20:45:50 +0200

    Task 001

---
+++ Seminars C#/Seminar 007/Task 001/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+1. метод инициализирующий матрицу
+2. метод распечатывающий матрицу
+3. метод получения числа с консоли
+*/
+
+
+// метод получения числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некоректное число. Повторите ввод:");
+        }
+    }
+    return result;
+}
+
+
+
+// метод инициализирующий матрицу
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+
+// метод распечатывающий матрицу
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int countOfRows = GetNumber("Введитее кол-во строк:");
+int countOfColumns = GetNumber("Введитее кол-во столбцов:");
+int[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+PrintMatrix(matrix);
