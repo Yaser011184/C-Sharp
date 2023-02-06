b4d1c9d129920c4635cb48e655f5ea20ff7fe238
Sergei <yagelavichus@gmail.com>
2023-02-06 20:59:31 +0200

    Task 002 m + n

---
+++ Seminars C#/Seminar 007/Task 002/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+1. инициализировать массив
+2. найти элементы в массиве
+3. вывести в консоль матрицу
+*/
+
+// 1. метод инициализирующий матрицу
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+
+    return matrix;
+}
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некоректное число. Повторите ввод:");
+        }
+    }
+    return result;
+}
+
+
+// вывести в консоль матрицу
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int countOfRows = GetNumber("Введитее кол-во строк:");
+int countOfColumns = GetNumber("Введитее кол-во столбцов:");
+
+int[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+PrintMatrix(matrix);
