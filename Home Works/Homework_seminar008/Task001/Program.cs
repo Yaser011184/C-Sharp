249a987af58ea5512e96d4804c9b50e141b084d7
Sergei <yagelavichus@gmail.com>
2023-02-12 15:58:47 +0200

    Task 001 Sort rows

---
+++ Home Works/Homework_seminar008/Task001/Program.cs
@@ -0,0 +1,92 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+
+int GetNumber(string message) // Получаем данные от пользователя
+{
+    int result = 0;
+    
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result ) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некоректное число. Повторите ввод:");
+        }        
+    }
+    return result;
+}
+
+int[,] InitMatrix(int rows, int columns) // Инициализируем двумерный массив(заполненный случайными числами)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix) // Выводим массив на консоль
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void SortRowsMatrix(int[,] matrix) // Сортирум строки массива по убыванию
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+      {
+        if (matrix[i, k] < matrix[i, k + 1])
+        {
+          int temp = matrix[i, k + 1];
+          matrix[i, k + 1] = matrix[i, k];
+          matrix[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+
+
+
+int rows = GetNumber("Введите количество строк: ");
+int columns = GetNumber("Введите количество столбцов: ");
+
+int[,] matrix = InitMatrix(rows, columns);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+SortRowsMatrix(matrix);
+PrintMatrix(matrix);
