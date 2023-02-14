278514aa2be5e64f9459cf924c1d1f8673b93981
Sergei <yagelavichus@gmail.com>
2023-02-14 20:56:34 +0200

    Task 001 N -> 1

---
+++ Home Works/Homework _seminar009/Task 001/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+
+string NumRec(int num)
+{
+    
+    if (num >= 1) return $"{num}, " + NumRec(num - 1);
+    else return string.Empty;
+}
+
+int numN = 8
+;
+
+Console.WriteLine(NumRec(numN));
