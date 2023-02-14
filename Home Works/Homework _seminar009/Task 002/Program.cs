58c1b72810452ffef51729234362dd36c9ec39ad
Sergei <yagelavichus@gmail.com>
2023-02-14 20:58:50 +0200

    Task 002 sum M -> N

---
+++ Home Works/Homework _seminar009/Task 002/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {   
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели некоректное число. Повторите ввод:");
+        }
+    }
+    return result;
+}
+
+
+int NumRec(int m, int n)
+{
+    int sum = 0;
+    if (m <= n) return sum += m + NumRec(m + 1, n);
+    else return 0;    
+}
+
+int m = GetNumber("Введите число:");
+int n = GetNumber("Введите число:");
+
+Console.WriteLine(NumRec(m, n));
